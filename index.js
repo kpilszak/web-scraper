7f249da61dc3c8dcdb25d4dc2491f6676004efa8
kpilszak <klaudia.pilszak@gmail.com>
2023-12-11 15:25:10 -0500

    Pare html data

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ const express = require('express');
 
 const app = express();
 
+const url = 'https://www.nytimes.com/';
+
+axios(url).then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $('.e1ppw5w20', html).each(function() {
+        const story = $(this).find('.story-wrapper .indicate-hover');
+        const title = story.text();
+        const url = $(this).find('a').attr('href');
+        articles.push({
+            title,
+            url
+        });
+    });
+
+    console.log(articles);
+}).catch(err => console.log(err));
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
