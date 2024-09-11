d6ab725d04c7f70c8d2426d7f8de96ae941c9573
kpilszak <klaudia.pilszak@gmail.com>
2024-09-10 20:37:15 -0400

    Use express get functions

--- index.js
+++ index.js
@@ -8,22 +8,27 @@ const app = express();
 
 const url = 'https://www.nytimes.com/';
 
-axios(url).then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const articles = [];
+app.get('/', function (req, res) {
+    res.json('This is the webscraper')
+})
 
-    $('.e1ppw5w20', html).each(function() {
-        const story = $(this).find('.story-wrapper .indicate-hover');
-        const title = story.text();
-        const url = $(this).find('a').attr('href');
-        articles.push({
-            title,
-            url
+app.get('/results', (req, res) => {
+    axios(url).then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = [];
+
+        $('.e1ppw5w20', html).each(function () {
+            const story = $(this).find('.story-wrapper .indicate-hover');
+            const title = story.text();
+            const url = $(this).find('a').attr('href');
+            articles.push({
+                title,
+                url
+            });
         });
-    });
-
-    console.log(articles);
-}).catch(err => console.log(err));
+        res.json(articles)
+    }).catch(err => console.log(err));
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
